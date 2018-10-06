914f5895d0bd2add49fea0faf7a3ac8b87058e0c
Jozef Randjak <randjakj@bluepartners.cz>
2018-10-06 13:56:30 +0200

    add on page logic in sessions

--- FrontEnd/Pages/Sessions.cshtml.cs
+++ FrontEnd/Pages/Sessions.cshtml.cs
@@ -22,9 +22,27 @@ namespace FrontEnd.Pages
 
         public int? Dayoffset { get; set; }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync(int id)
         {
+            Session = await _apiClient.GetSessionAsync(id);
 
+            if (Session == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            var allSessions = await _apiClient.GetSessionsAsync();
+
+            var startDate = allSessions.Min(s => s.StartTime?.Date);
+
+            Dayoffset = Session.StartTime?.Subtract(startDate ?? DateTimeOffset.MinValue).Days;
+
+            if (!string.IsNullOrEmpty(Session.Abstract))
+            {
+                Session.Abstract = "<p>" + String.Join("</p><p>", Session.Abstract.Split("\r\n", StringSplitOptions.RemoveEmptyEntries)) + "</p>";
+            }
+
+            return Page();
         }
     }
 }
