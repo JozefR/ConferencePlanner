773c6e1d7a1f352608d4a4d5183e882ab5e652fe
Jozef Randjak <randjakj@bluepartners.cz>
2018-10-06 13:51:34 +0200

    Add Sessions page to frontend

---
+++ FrontEnd/Pages/Sessions.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferenceDTO;
+using FrontEnd.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontEnd.Pages
+{
+    public class SessionModel : PageModel
+    {
+        private readonly IApiClient _apiClient;
+
+        public SessionModel(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public SessionResponse Session { get; set; }
+
+        public int? Dayoffset { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ FrontEnd/workflow.md
@@ -0,0 +1,12 @@
+###Workflow .core command
+
+* Add new razor template with FrontEnd folder
+
+        dotnet new razor -o FrontEnd
+        
+* Add reference 
+
+        dotnet add reference ../ConferenceDTO/ConferenceDTO.csproj
+        
+        
+        
