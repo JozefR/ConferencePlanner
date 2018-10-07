1a3c7f70b2425becb23fe8379ba1330a6df62617
Jozef Randjak <randjakjozef@gmail.com>
2018-10-07 17:28:56 +0200

    add speaker page into frontend

---
+++ FrontEnd/Pages/Speaker.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferenceDTO;
+using FrontEnd.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontEnd.Pages
+{
+    public class SpeakerModel : PageModel
+    {
+        private readonly IApiClient _apiClient;
+
+        public SpeakerModel(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public SpeakerResponse Speaker { get; set; }
+        
+        public void OnGet()
+        {
+            
+        }
+    }
+}
